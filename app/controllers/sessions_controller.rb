b9cb7237dc959af3a3b7b5c4a2f03cd012532168
Chad Lengefeld <chadlengefeld@gmail.com>
2021-01-26 08:42:17 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,10 @@ class SessionsController < ApplicationController
       if !params[:name]
         redirect_to '/sessions/new'
 
-    elsif params[:name] = nil || params[:name].empty?
+    elsif params[:name] = nil 
        redirect_to '/'
     else
+      params[:name].empty?
     # if params[:name].nil?
     #   params[:name].empty? || !params[:name]
       redirect_to '/'
